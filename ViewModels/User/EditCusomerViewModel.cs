efc83ad90ce16569342dc96af8159e089db03c60
Ibrahim Mansou <ibrahim.mansour3010@gmail.com>
2021-12-24 16:10:14 +0200

    edit profile

--- ViewModels/User/EditCusomerViewModel.cs
+++ ViewModels/User/EditCusomerViewModel.cs
@@ -18,7 +18,6 @@ namespace ViewModels.Customer
 
         public string Email { get; set; }
 
-        public string Username { get; set; }
         public string Password { get; set; }
         public Gender Gender { get; set; }
         public string Address { get; set; }
--- ViewModels/User/GetUserViewModel.cs
+++ ViewModels/User/GetUserViewModel.cs
@@ -20,7 +20,6 @@ namespace ViewModels.User
 
         public string Username { get; set; }
 
-        public string Password { get; set; }
         public Gender Gender { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
